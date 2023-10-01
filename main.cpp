0bd5795672e3c03585d4ae2e27d1aea0bb858437
OdeOmega <agustinhermosillabaez@gmail.com>
2023-10-01 16:39:10 -0300

    Commit

--- main.cpp
+++ main.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
+#include <vector>
+#include<list>
+using namespace std;
+
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    //lalalalla
-    return 0;
+    int cantTest, edificios, cantConexiones,conex,ed1,ed2,d,r;
+    cin >> cantTest;
+    for(int i = 0; i<cantTest;i++){
+        cin >> edificios >> cantConexiones;
+        vector<list<vector<int>>> conexiones(edificios+1);
+        for(int j = 0; j<cantConexiones;j++){
+            cin >> ed1 >> ed2 >> d >> r;
+            conexiones[ed1].push_back({ed2,d,r});
+            conexiones[ed2].push_back({ed1,d,r});
+        }
+        int b = 0;
+    }
+    int a = 0;
+
 }
