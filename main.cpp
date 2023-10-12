117c93279717966ef3a6ac6979acec929911c5fe
OdeOmega <agustinhermosillabaez@gmail.com>
2023-10-12 15:32:23 -0300

    va queriendo

--- main.cpp
+++ main.cpp
@@ -5,62 +5,95 @@
 #include <algorithm>
 using namespace std;
 
-long minInf = LONG_MIN;
+struct triple {
+    int first;
+    int second;
+    int third;
+}
 
-struct sextuple {
-    float first;
+struct quintuple {
+    int first;
     int second;
     int third;
     int fourth;
     int fifth;
-    int sixth;
 
-    bool operator<(const sextuple& other) const {
+    bool operator<(const quintuple& other) const {
         if (first > other.first)
             return true;
         else if (first < other.first)
             return false;
         return second < other.second;
     }
+
 };
 
-pair<int, int> operator+(const pair<int, int>& a, const pair<int, int>& b) {
-    int sum_first = a.first + b.first;
-    int sum_second = a.second + b.second;
-    return make_pair(sum_first, sum_second);
-}
+struct DSU {
+    vector<int> padre;
+    vector<int> tam;
 
-
-pair<int,int> maximizar(vector<sextuple> conexiones, vector<bool> usado){
-    int R = 0;
-    int D = 0;
-    for(int i = 0; i < conexiones.size();i++){
-        if(!usado[conexiones[i].third] || !usado[conexiones[i].fourth]){
-            usado[conexiones[i].third] = true;
-            usado[conexiones[i].fourth] = true;
-            D = D+conexiones[i].fifth;
-            R = R+conexiones[i].sixth;
+    DSU(int n){
+        padre = vector<int>(n);
+        tam = vector<int>(n,0);
+        for(int i = 0; i<n;i++){
+            padre[i] = i;
         }
     }
-    return {D,R};
-}
+    int find(int v){
+        if (padre[v] != v){
+            padre[v] = find(padre[v]);
+        }
+        return padre[v];
+    }
 
+    void unite (int a, int b){
+        a = find(a);
+        b = find(b);
+        if(tam[a] < tam[b]){
+            swap(a,b);
+        }
+        padre[b] = a;
+        tam[a] = tam[a]+1;
+    }
+
+};
+
+triple kruskal(vector<quintuple> conexiones, int n){
+    long long C = 0;
+    long long D = 0;
+    long long R = 0;
+    sort(conexiones.begin(),conexiones.end());
+    DSU DSU(n);
+
+    int aristas = 0;
+
+    for(int i = 0; i<conexiones.size(); i++){
+        if(DSU.find(conexiones[i].second) != DSU.find(conexiones[i].third)){
+            DSU.unite(conexiones[i].second,conexiones[i].third);
+            C = C + conexiones[i].first;
+            D = D + conexiones[i].fourth;
+            R = R + conexiones[i].fifth;
+            aristas++;
+        }
+    }
+
+}
 
 int main() {
     int cantTest, cantEdificios, cantConexiones, ed1, ed2, d, r;
     cin >> cantTest;
     for (int i = 0; i < cantTest; i++) {
         cin >> cantEdificios >> cantConexiones;
-        vector<sextuple> conexiones;
+        vector<quintuple> conexiones;
+        int C = 1;
         for (int j = 0; j < cantConexiones; j++) {
             cin >> ed1 >> ed2 >> d >> r;
-            float div = static_cast<float>(d) / static_cast<float>(r);
-            div = div / static_cast<float>(d-r);
-            conexiones.push_back({div,d-r,ed1-1,ed2-1,d,r});
+            conexiones.push_back({d-C*r,ed1,ed2,d,r});
         }
-        sort(conexiones.begin(),conexiones.end());
-        vector<bool> usado(cantEdificios,false);
-        pair<int,int> ANS = maximizar(conexiones,usado);
-        cout << ANS.first << " " << ANS.second << endl;
+        triple ANS = kruskal(conexiones, C);
+        while(C != ANS.first){
+            C = ANS.first;
+            ANS = kruskal(conexiones,)
+        }
     }
 }
